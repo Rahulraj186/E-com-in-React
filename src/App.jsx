e31ee9b5d445e390cb78f555884282c84ee5c085
rahul <rahulraj1860@gmail.com>
2025-09-06 17:04:32 +0530

    clean up

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,9 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
-import { Container } from "react-bootstrap";
-import { Home } from "./Pages/Home";
-import { About } from "./Pages/About";
-import { Store } from "./Pages/Store";
-
-import Header from "./Components/Header";
-import { ShoppingCartProvider } from "./Context/ShoppingCartContext";
-import ContactUs from "./Pages/ContactUs";
+import React from 'react'
 
 const App = () => {
   return (
-    <ShoppingCartProvider>
-      <Header />
-      <Container>
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/Store" element={<Store />}></Route>
-          <Route path="/About" element={<About />}></Route>
-          <Route path="/ContactUs" element={<ContactUs />}></Route>
-        </Routes>
-      </Container>
-    </ShoppingCartProvider>
-  );
-};
+    <div>App</div>
+  )
+}
 
-export default App;
+export default App
